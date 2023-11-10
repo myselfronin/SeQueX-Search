5acd4ec5d4b06b69bad96090007242d4b632a6f0
Rabinson Ghatani <myself.ronin@hotmail.com>
2023-11-10 08:55:45 +0100

    Add intial form request and log

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+from flask_wtf.csrf import CSRFProtect
 
+load_dotenv()
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+csrf = CSRFProtect(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
 from app import app
-from flask import render_template
+from flask import request, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
-@app.route("/")
+class SearchForm(FlaskForm):
+    query = StringField('Query', validators=[DataRequired()])
+
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("home.html")
+    form = SearchForm()
+    return render_template("home.html", form=form)
+
+@app.route("/", methods=["POST"])
+def search():
+    form = SearchForm()
+    if form.validate_on_submit():
+        query = form.query.data
+        print(f"User query: {query}")
+        return f'Searching for: {query}'
+    else:
+        return render_template("home.html", form=form)
