35662e8c01e4cccdb9df0a2feeecf057ec324a12
Rabinson Ghatani <myself.ronin@hotmail.com>
2023-12-04 22:34:17 +0100

    Fixed command for publication indexing in solr

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -4,6 +4,7 @@ from .aida.pull_paper_syntactic_topics import pull_paper_syntactic_topics
 from .make_annotated_dataset import make_annotated_dataset
 from .spacy.prepare_dataset import prepare_dataset
 from .solr.index_cso_topics import solr_index_topics
+from .solr.solr_index_publications import solr_index_papers
 
 def register_commands(app):
     app.cli.add_command(pull_cs_papers)
@@ -12,3 +13,4 @@ def register_commands(app):
     app.cli.add_command(make_annotated_dataset)
     app.cli.add_command(prepare_dataset)
     app.cli.add_command(solr_index_topics)
+    app.cli.add_command(solr_index_papers)
