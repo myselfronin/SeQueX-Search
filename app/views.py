e490bd8d398b0ddcbb9fdd16883f52fdeb40d92e
Rabinson Ghatani <myself.ronin@hotmail.com>
2023-12-04 22:07:52 +0100

    update page design

--- app/views.py
+++ app/views.py
@@ -1,13 +1,14 @@
 from app import app
 from flask import request, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, RadioField
 from wtforms.validators import DataRequired
 from app.services.ner import NamedEntityRecognition
 import spacy
 import os
 class SearchForm(FlaskForm):
     query = StringField('Query', validators=[DataRequired()])
+    search_option = RadioField('Search Option', choices=[('sqe', 'Semantic Query Expansion'), ('keyword', 'Keyword based')], default='sqe', validators=[DataRequired()])
 
 
 @app.route("/", methods=["GET"])
@@ -15,22 +16,22 @@ def index():
     form = SearchForm()
     return render_template("home.html", form=form)
 
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html", form=form)
+
+@app.route("/search", methods=["GET", "POST"])
 def search():
     form = SearchForm()
     if form.validate_on_submit():
+        print(form.search_option.data)
         query = form.query.data
         ner = NamedEntityRecognition()
-        
+
         entities = ner.get_entities(query)
         for ent in entities:
             print(ent)
-        # print(os.path.exists('storage/model-best'))
-        # nlp = spacy.load('storage/model-best')
-        # doc = nlp(query)
-        # for ent in doc.ents:
-        #     print(f"- {ent.text} ({ent.label_})")
-        # print(f"User query: {query}")
-        return f'Searching for: {query}'
+        
+        return render_template("search.html", form=form)
     else:
         return render_template("home.html", form=form)
