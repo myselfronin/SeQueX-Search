2bda6696700e518d8fd1fda347c94aeaed433c01
Rabinson Ghatani <myself.ronin@hotmail.com>
2023-12-18 22:34:22 +0100

    Added local disambiguator with context and name entitiy similarity and then calculate ranking

--- app/components/candidate_entity_disambiguation/__init__.py
+++ app/components/candidate_entity_disambiguation/__init__.py
@@ -1,4 +1,5 @@
 from .ranking import CandidateRanking
+from .local_disambiguation import LocalDisambiguation
 from app.models import Topics
 from app import logger
 
@@ -12,6 +13,7 @@ class CandidateDisambiguator:
         self.query_context = query_context
         self.mentions_with_candidate_set = mentions_with_candidate_set
         self.candidate_ranker = CandidateRanking()
+        self.local_disambiguator = LocalDisambiguation()
 
     def disambiguate(self, mention_label):
         """
@@ -31,8 +33,27 @@ class CandidateDisambiguator:
             .filter(Topics.cso_uri.in_(candidate_uris)).all()
         )
 
+        # Disambiguation Score
+        score_card = {}
+
+        # Local Disambiguation Scores
+        for uri, label, description in topics_with_description:
+            # If the description is empty then just assign label and caculate similarity based on that
+            description_text = description if description is not None else label
+
+            # Context Similarity Calculation
+            context_similarity_score = self.local_disambiguator.context_similarity(self.query_context, description_text)
+
+            # Named Entity Similairty Calculation
+            named_entity_similarity_score = self.local_disambiguator.entity_name_similarity(mention_label, label)
+
+            score_card[uri] = {
+                'context_similarity_score': context_similarity_score, 
+                'named_entity_similarity_score': named_entity_similarity_score
+            }
+
         # Rank the Candidate
-        ranked_candidates = self.candidate_ranker.rank_candidates(self.query_context, topics_with_description)
+        ranked_candidates = self.candidate_ranker.rank_candidates(score_card)
 
         # Return the top candidate if the list is not empty
         if ranked_candidates:
--- app/components/candidate_entity_disambiguation/ranking.py
+++ app/components/candidate_entity_disambiguation/ranking.py
@@ -1,24 +1,17 @@
-from .semantic_similarity import SemanticSimilarity
-
 class CandidateRanking:
-    def __init__(self):
-        self.similarity_calculator = SemanticSimilarity()
 
-    def rank_candidates(self, query_context, candidates_with_description):
+    def rank_candidates(self, score_card):
         """
-        Rank candidates based on their cosine similarity to the query.
+        Rank candidates based on the sum of context similarity and named entity similarity scores.
 
-        :param query_context: The query text
-        :param candidates_with_description: A list of tuples, each containing (URI, label, description) of a candidate
-        :return: A list of tuples (candidate URI, label, score) sorted by score in descending order
+        :param score_card: Dictionary containing the scores for each candidate.
+        :return: A list of tuples (candidate URI, total score) sorted by total score in descending order.
         """
-        rankings = []
-        for uri, label, description in candidates_with_description:
-            # If the description is empty then just assign label and caculate similarity based on that
-            description_text = description if description is not None else label
-            score = self.similarity_calculator.cosine_similarity(query_context, description_text)
-            rankings.append((uri, score))
+        total_scores = []
+        for uri, scores in score_card.items():
+            total_score = scores['context_similarity_score'] + scores['named_entity_similarity_score']
+            total_scores.append((uri, total_score))
 
-        # Sort the candidates based on the similarity score in descending order
-        rankings.sort(key=lambda x: x[1], reverse=True)
-        return rankings
+        # Sort the candidates based on the total score in descending order
+        total_scores.sort(key=lambda x: x[1], reverse=True)
+        return total_scores
