a9dbf925f140156376a3dd256918a781166ebc46
Rabinson Ghatani <myself.ronin@hotmail.com>
2023-12-18 22:34:37 +0100

    Add logs of the steps

--- app/views.py
+++ app/views.py
@@ -4,8 +4,9 @@ from app.services.ner import NamedEntityRecognition
 from app.services.solr import SolrService
 from app.components.query_preprocessor import QueryPreprocessor
 from app.components.canditate_entity_generator import CandidateEntityGenerator
-import spacy
-import os
+from app.components.candidate_entity_disambiguation import CandidateRanking
+from app.components.candidate_entity_disambiguation import CandidateDisambiguator
+from app import logger
 
 PAGE_SIZE=10
 
@@ -13,34 +14,69 @@ PAGE_SIZE=10
 def index():
     return render_template("home.html")
 
+
 @app.route("/search", methods=["GET"])
 def search():
     page = request.args.get('page', 1, type=int)
     query = request.args.get('query', '')
     search_option = request.args.get('search_option', 'sqe') 
+    start = (page - 1) * PAGE_SIZE 
 
+    # SQE based search
     if search_option == 'sqe':
-        # SQE based search
-        ner = NamedEntityRecognition()
+        #--------
+        logger.info('\n\n\n\nSQE SEARCH')
+        logger.info('Query Text: '+ query)
+        #--------
+
+        ### Preprocess Query ###
         processed_query = QueryPreprocessor().preprocess(query)
+
+        #--------
+        logger.info('Processed Query: ' + processed_query)
+        #--------
+
+        ### Named Entity Recognition ###
+        ner = NamedEntityRecognition()
         entities, corrected_dict = ner.get_entities(processed_query)
+
+        ### Corrected query spelling if any found ###
         corrected_query = replace_corrected_entities(query, corrected_dict)
         ner_highlighted_text = highlight_entities(corrected_query, entities)
 
-        #Candidate Terms
-        candidate_entities = CandidateEntityGenerator(entities).get_candidate_from_topic_indexes()
+        #--------
+        logger.info('Recognized Entities: ' + ', '.join(entities))
+        logger.info('Corrected query: ' + corrected_query)
+        #--------
 
-        return render_template("search.html", documents=[], total_results=0, time_taken=0, page=1, query=query, search_option=search_option, ner_highlighted_text=ner_highlighted_text, recognized_entities=entities, candidate_entities=candidate_entities)
+        ### Candidate Generation ###
+        reg_mentions_candidate_set = CandidateEntityGenerator(entities).get_candidates()
+
+        #--------
+        log_candidates_set(reg_mentions_candidate_set)
+        #--------
+
+        ### Entity Diambiguation ###
+        disambiguator = CandidateDisambiguator(corrected_query, reg_mentions_candidate_set)
+        linked_entities = { mention: disambiguator.disambiguate(mention) for mention in reg_mentions_candidate_set}
+
+        #--------
+        formatted_pairs = [f"Entity Linking: '{key}': {value}" for key, value in linked_entities.items()]
+        logger.info('\n' + '\n'.join(formatted_pairs))
+        #--------
+
+        exit()
+        #Search in Solr
+        results = semantic_search(candidate_entities, start=start)
+
+
+        return render_template("search.html", documents=results['documents'], total_results=results['total_results'], time_taken=results['time_taken'], page=1, query=query, search_option=search_option, ner_highlighted_text=ner_highlighted_text, recognized_entities=entities, candidate_entities=candidate_entities)
     
     else: 
         # Keyword based search
-        start = (page - 1) * PAGE_SIZE  # Assuming 10 results per page
         results = keyword_search(query, start=start)
-        total_results = results['total_results']
-        time_taken = results['time_taken']
-        documents = results['documents']
 
-        return render_template("search.html", documents=documents, total_results=total_results, time_taken=time_taken, page=page, query=query, search_option=search_option)
+        return render_template("search.html", documents=results['documents'], total_results=results['total_results'], time_taken=results['time_taken'], page=page, query=query, search_option=search_option)
         
 
 def keyword_search(query, start=0, rows=PAGE_SIZE):
@@ -51,14 +87,22 @@ def keyword_search(query, start=0, rows=PAGE_SIZE):
 
     # Construct solr query to search in abstract and title
     solr_query = solr_service.make_keyword_based_query(keywords)
-    print(solr_query)
 
     # Execute the query in the solr engine
     results = solr_service.get_paper_matches(solr_query, start=start, rows=rows)
 
     return results
 
+def semantic_search(entities, start=0, rows=PAGE_SIZE):
+    # Construct solr query to search in abstract and title
+    solr_service = SolrService()
 
+    solr_query = solr_service.make_keyword_based_query(entities)
+
+    # Execute the query in the solr engine
+    results = solr_service.get_paper_matches(solr_query, start=start, rows=rows)
+
+    return results
 
 def highlight_entities(text, entities):
     """
@@ -85,3 +129,12 @@ def replace_corrected_entities(query, corrected_dict):
     for incorrect, corrected in corrected_dict.items():
         query = query.replace(incorrect, corrected)
     return query
+
+
+def log_candidates_set(candidate_set):
+    # Create a list of formatted strings
+    formatted_entries = [f"Recognized Entity: '{entity}', Candidate Entities: {', '.join(candidates)}" 
+                        for entity, candidates in candidate_set.items()]
+    # Join the formatted strings and log them
+    logger.info('\n'.join(formatted_entries))
+    
